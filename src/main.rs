b34c6ccd2735d1575d45618af2f0f0f4265cef84
Kazuki Yamada <koukun0120@gmail.com>
2020-05-31 00:07:30 +0900

    update: guess

--- src/main.rs
+++ src/main.rs
@@ -11,21 +11,23 @@ fn main() {
 
     println!("The secret number is: {}", secret_number);    //秘密の数字は次の通り: {}
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number!");                 //数値を入力してください！
+        let guess: u32 = guess.trim().parse()
+            .expect("Please type a number!");
 
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),       //小さすぎ！
-        Ordering::Greater => println!("Too big!"),      //大きすぎ！
-        Ordering::Equal => println!("You win!"),        //やったね！
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("You win!"),
+        }
     }
 }

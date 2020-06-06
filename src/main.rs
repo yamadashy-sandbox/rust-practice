9d6b5557533bf3342d1a9fa97ecf173a54609046
Kazuki Yamada <koukun0120@gmail.com>
2020-06-06 22:52:33 +0900

    update: valiable

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,6 @@
-extern crate rand;
-
-use std::io;
-use std::cmp::Ordering;
-use rand::Rng;
-
 fn main() {
-    println!("Guess the number!");
-
-    let secret_number = rand::thread_rng().gen_range(1, 101);
-
-    println!("The secret number is: {}", secret_number);    //秘密の数字は次の通り: {}
-
-    loop {
-        println!("Please input your guess.");
-
-        let mut guess = String::new();
-
-        io::stdin().read_line(&mut guess)
-            .expect("Failed to read line");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        println!("You guessed: {}", guess);
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
-        }
-    }
+    let x = 5;
+    println!("The value of x is: {}", x);     // xの値は{}です
+    // x = 6;
+    // println!("The value of x is: {}", x);
 }

3991204446efe43951f00c9a9df17c758e4cf705
Kazuki Yamada <koukun0120@gmail.com>
2020-06-10 23:48:41 +0900

    update: function

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
-    let x: (i32, f64, u8) = (500, 6.4, 1);
+    println!("Hello, world!");
 
-    let five_hundred = x.0;
+    another_function();
+}
 
-    let six_point_four = x.1;
-
-    let one = x.2;
+fn another_function() {
+    println!("Another function.");  // 別の関数
 }
